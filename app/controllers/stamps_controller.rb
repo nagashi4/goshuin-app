f2e9402a634604e2c8b44c0bd3d7b11702750414
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2022-02-09 23:14:43 +0900

    投稿機能の実装

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,4 +1,28 @@
 class StampsController < ApplicationController
   def index
+    
+  end
+
+  def new
+    @stamp = Stamp.new
+  end
+   
+  def create
+    @stamp = Stamp.new(stamp_params)
+    if @stamp.save
+      redirect_to root_path
+
+    else
+      render :new
   end
 end
+
+
+
+  private  
+
+  def stamp_params
+    params.require(:stamp).permit(:title, :prefecture, :place, :image).merge(user_id: current_user.id)
+  end
+end
+
